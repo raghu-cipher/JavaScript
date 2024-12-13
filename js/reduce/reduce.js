5fec97c26c99ad7cc5071123007164ebdf1706fd
raghu-cipher <raghunow3@gmail.com>
2024-12-13 19:56:10 +0530

    -

--- js/reduce/reduce.js
+++ js/reduce/reduce.js
@@ -1,15 +1,15 @@
-// .reduce() = reduce the element of an array to a 
+// reduce() = reduce the element of an array to a 
 //             single value 
 
 
-// const prices = [5,30,10,25,15,20];
+const prices = [5,30,10,25,15,20];
 
-// const total = prices.reduce(sum);
-// console.log(total);
+const total = prices.reduce(sum);
+console.log(total);
 
-// function sum(accumulator,element) {
-//     return accumulator + element ;
-// } 
+function sum(accumulator,element) {
+    return accumulator + element ;
+} 
 
 const grades = [75,50,90,80,65,95];
 
