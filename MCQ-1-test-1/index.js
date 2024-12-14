d349b2a2446279936d5ddff407a1a68925c57b6e
raghu-cipher <raghunow3@gmail.com>
2024-12-14 19:53:14 +0530

    -

--- MCQ-1-test-1/index.js
+++ MCQ-1-test-1/index.js
@@ -1,125 +1,129 @@
 // console.log("this mcq test--1-1");
 
-// let person = {
-//     "first name" : "Alice",
-//     "last name" : "Jane",
-//     age : 28,
-// };
-// let key = "last name";
-// let key2 = "first name";
-// console.log(person["first name"]); // object lo key kinda chustundi
-// console.log(person[key]); // ila iste oka variable kinda define itadi 
+let person = {
+    "first name" : "Alice",
+    "last name" : "Jane",
+    age : 28,
+};
+let key = "last name";
+let key2 = "first name";
+console.log(person["first name"]); // object lo key kinda chustundi
+console.log(person[key]); // ila iste oka variable kinda define itadi 
 
 // // adding elements to the object; 
 
-// person.designation = "abxdef";
-// person["mobileNumber"] = "123456789"
-// console.log(person)
+person.designation = "abxdef";
+person["mobileNumber"] = "123456789"
+console.log(person)
 
-// let mblNmbr = "mobileNumber" ;
-// console.log(person[mblNmbr]); // 
+//--------------------------------
 
-// person.mobileNumber = "98765432"; // already key unte value ni update chestundi
-// console.log(person);
+let mblNmbr = "mobileNumber" ;
+console.log(person[mblNmbr]); // 
+
+person.mobileNumber = "98765432"; // already key unte value ni update chestundi
+console.log(person);
 
 
-// let myArray = [97, 252, "Mobile", "Laptop"];
-// let result = myArray.find(function (arrayItem) {
-//     if(arrayItem === "Mobile") {
-//         return true ;
-//     }else if(arrayItem === 97){
-//         return true ;
-//     }else {
-//         return false ;
-//     }
-// })
+let myArray = [97, 252, "Mobile", "Laptop"];
+let result = myArray.find(function (arrayItem) {
+    if(arrayItem === "Mobile") {
+        return true ;
+    }else if(arrayItem === 97){
+        return true ;
+    }else {
+        return false ;
+    }
+})
 
-// console.log(result);
+console.log(result);
 
 // 3 
 
-// let myArray = [4,5,6];
+let myArray1 = [4,5,6];
 
-// myArray.unshift(3);
-// console.log(myArray);
+myArray1.unshift(3);
+console.log(myArray1);
 
-// myArray.shift();
-// console.log(myArray)
+myArray1.shift();
+console.log(myArray1)
 
-// myArray.unshift(1,2);
-// console.log(myArray);
+myArray1.unshift(1,2);
+console.log(myArray1);
 
 // 4
 
-// let laptopDevices = ["Mouse", "CPU","keyboard","Screen","Printer"];
-// laptopDevices.shift();
-// console.log(laptopDevices.shift());
-// console.log(laptopDevices);
+let laptopDevices = ["Mouse", "CPU","keyboard","Screen","Printer"];
+laptopDevices.shift();
+console.log(laptopDevices.shift());
+console.log(laptopDevices);
 
 //5
 
-// let profile = {
-//     name : "akhira",
-//     age : 29,
-//     designation : "Web Developer"
-// };
-// console.log(typeof(profile));
-// console.log(typeof(JSON.stringify(profile)));
+let profile = {
+    name : "akhira",
+    age : 29,
+    designation : "Web Developer"
+};
+console.log(typeof(profile));
+console.log(typeof(JSON.stringify(profile)));
 
 // 6
 
-// let a = [1,2,3];
-// a[4] = 4;
-// console.log(a);
-// console.log(a[3]); 
-// console.log(a[4]); 
+let a = [1,2,3];
+a[4] = 4;
+console.log(a);
+console.log(a[3]); 
+console.log(a[4]); 
 
 //7
 
-// let score = 149 ;
-// let stringifiedScore = JSON.stringify(score);
+let score = 149 ;
+let stringifiedScore = JSON.stringify(score);
 
-// console.log(typeof(stringifiedScore));
+console.log(typeof(stringifiedScore));
 
-// function parser() {
-//     let parsedData = JSON.parse(stringifiedScore);
-//     console.log(typeof(parsedData));
-// }
-// parser();
+function parser() {
+    let parsedData = JSON.parse(stringifiedScore);
+    console.log(typeof(parsedData));
+}
+parser();
 
 // // 8
-// console.log(false == "0"); // true 
-// console.log(false === 1);  // false
+
+console.log(false == "0"); // true 
+console.log(false === 1);  // false
 
 // 9 
 
-// let myArray = [5,"six",2,8.2];
-// myArray.splice(2,0,"one", false);
+let myArray2 = [5,"six",2,8.2];
+myArray2.splice(2,0,"one", false);
 
-// console.log(myArray);
+console.log(myArray2);
 
-// myArray.slice(2,myArray.length);
+myArray2.slice(2,myArray2.length);
 
-// console.log(myArray[5]); // 8.2
+console.log(myArray2[5]); // 8.2
 
 // 10 
-// let teamOne = "50";
-// let teamTwo = "20";
 
-// let result = parseInt(teamTwo) + 100 ;
+let teamOne = "50";
+let teamTwo = "20";
 
-// console.log(result); // 120 
-// console.log(result + teamOne) ; // 12050
+let result1 = parseInt(teamTwo) + 100 ;
+
+console.log(result1); // 120 
+console.log(result1 + teamOne) ; // 12050
 
 // 11  ---> if there is no item it retruns -1 ; 
 
 
-// let squareNumbers = [12, 27, 56, 83];
+let squareNumbers = [12, 27, 56, 83];
 
-// squareNumbers.push(27);
-// console.log(squareNumbers.lastIndexOf(27)); // 4 
-// console.log(squareNumbers);
+squareNumbers.push(27);
+console.log(squareNumbers.lastIndexOf(27)); // 4 
+console.log(squareNumbers);
 
-// squareNumbers.pop();
-// console.log(squareNumbers.lastIndexOf(27)); // 1
+squareNumbers.pop();
+console.log(squareNumbers.lastIndexOf(27)); // 1
 
--- MCQ-test-1/index.js
+++ MCQ-test-1/index.js
@@ -1,51 +1,51 @@
-// const camera = {
-//     brand : "Nikon",
-//     magapixel : 48,
-//     aperture : 1.8,
-//     focalLength : 200,
-//     cost : 40000
-// }
-// const {aperture,focalLength, ...rest} = camera ;
-// console.log(rest.aperture)
-// console.log(aperture)
-// console.log(rest)
+const camera = {
+    brand : "Nikon",
+    magapixel : 48,
+    aperture : 1.8,
+    focalLength : 200,
+    cost : 40000
+}
+const {aperture,focalLength, ...rest} = camera ;
+console.log(rest.aperture)
+console.log(aperture)
+console.log(rest)
 
-// const stationary = ["Pencil","Eraser","Stapler"]
-// const store = stationary;
+const stationary = ["Pencil","Eraser","Stapler"]
+const store = stationary;
 
-// console.log(store)
-// const bag = [...stationary]
+console.log(store)
+const bag = [...stationary]
 
-// console.log(`${stationary[0]}---${store[0]}--${bag[0]}`)
-// bag[0] = "Pen";
-// bag[0] = "red";
+console.log(`${stationary[0]}---${store[0]}--${bag[0]}`)
+bag[0] = "Pen";
+bag[0] = "red";
 
-// console.log(`${stationary[0]}--${bag[0]}`)
-// console.log(bag)
-
-
-// const positiveIntegers = [2,3,4] ;
-// function getProduct(a,b,c = 2, d = 3) {
-//     return a*b*c*d ;
-// }
-// console.log(getProduct(...positiveIntegers));
+console.log(`${stationary[0]}--${bag[0]}`)
+console.log(bag)
 
 
-// const compositNumbers = [2,4,6,8,9];
+const positiveIntegers = [2,3,4] ;
+function getProduct(a,b,c = 2, d = 3) {
+    return a*b*c*d ;
+}
+console.log(getProduct(...positiveIntegers));
 
-// function getProduct(a,b,rest) {
-//     return a*b*rest ;
+
+const compositNumbers = [2,4,6,8,9];
+
+function getProduct(a,b,rest) {
+    return a*b*rest ;
     
-// }
+}
 
-// console.log(getProduct(...compositNumbers));
+console.log(getProduct(...compositNumbers));
 
 
-// const room1 ={lenght : 300, height : 200};
-// const room2 = {lenght : 100, width : 150,};
-// const room3 = {...room1,...room2};
-// console.log(room3)
-// console.log(Object.getOwnPropertyNames(room3))
+const room1 ={lenght : 300, height : 200};
+const room2 = {lenght : 100, width : 150,};
+const room3 = {...room1,...room2};
+console.log(room3)
+console.log(Object.getOwnPropertyNames(room3))
 
 const convertToObject1 = (item, category) => ({item,category});
 
@@ -54,51 +54,51 @@ const convertToObject2 = (item,category) => {item,category};
 console.log(convertToObject1("Face Cream", "Cosmotics"));
 console.log(convertToObject2("Face Cream", "Cosmotics"));
 
-// let apple = {
-//     cost : 5
-// };
-// console.log(apple)
+let apple = {
+    cost : 5
+};
+console.log(apple)
 
-// let banana = apple;
-// let mango = {
-//     cost : 20 
-// };
-// banana.cost = 10 
+let banana = apple;
+let mango = {
+    cost : 20 
+};
+banana.cost = 10 
 
-// console.log(apple.cost)
-// console.log(banana.cost)
-// console.log(mango.cost)
+console.log(apple.cost)
+console.log(banana.cost)
+console.log(mango.cost)
 
-// banana = mango 
+banana = mango 
 
-// console.log(banana.cost,apple.cost)
-// console.log(apple,banana)
+console.log(banana.cost,apple.cost)
+console.log(apple,banana)
 
-// const arr1 = [1,2,3];
-// const arr2 = [[...arr1],4,5,6];
+const arr1 = [1,2,3];
+const arr2 = [[...arr1],4,5,6];
 
-// console.log(arr2[1])
+console.log(arr2[1])
 
-// const ceiling1 = {color : "blue", texture : "smooth"};
-// const ceiling12 = {rating : 10, texture : "rough"};
-// const ceiling3 = {...ceiling1, ...ceiling12};
-// const ceiling4 = {...ceiling1, rating : 12};
+const ceiling1 = {color : "blue", texture : "smooth"};
+const ceiling12 = {rating : 10, texture : "rough"};
+const ceiling3 = {...ceiling1, ...ceiling12};
+const ceiling4 = {...ceiling1, rating : 12};
 
-// console.log(ceiling3)
-// console.log(ceiling4)
-// console.log(ceiling4.color);
-// console.log(ceiling4.texture);
-// console.log(ceiling4.rating); 
+console.log(ceiling3)
+console.log(ceiling4)
+console.log(ceiling4.color);
+console.log(ceiling4.texture);
+console.log(ceiling4.rating); 
 
 
-// const myArray = ["x","y","z"];
-// const getLength = (...args) => {
+const myArray = ["x","y","z"];
+const getLength = (...args) => {
    
-//     console.log(args.length);
-//     console.log(typeof(args),args)
-// }
+    console.log(args.length);
+    console.log(typeof(args),args)
+}
 
-// console.log(getLength(...myArray))
+console.log(getLength(...myArray))
 
 const pen = {
     color : "Blue",
@@ -110,32 +110,32 @@ const pen = {
 pen.wirte();
 
 
-// const myArray = [["subItem1"], "item2", "item3", "item4"];
-// const [item1, item2] = myArray ;
+const myArray1 = [["subItem1"], "item2", "item3", "item4"];
+const [item1, item2] = myArray1 ;
 
-// console.log(item1, item2)
-// const [subItem1, subItem2] = item1 ;
+console.log(item1, item2)
+const [subItem1, subItem2] = item1 ;
 
-// console.log(subItem1);
-// console.log(subItem2) ;
+console.log(subItem1);
+console.log(subItem2) ;
 
-// const chef = {
-//     name : "Peter",
-//     cook : () => {
-//         console.log(this);
-//     },
-//     cook1 () {
-//         console.log(this)
-//     }
-// }
-// chef.cook();
-// chef.cook1();
+const chef = {
+    name : "Peter",
+    cook : () => {
+        console.log(this);
+    },
+    cook1 () {
+        console.log(this)
+    }
+}
+chef.cook();
+chef.cook1();
 
-// function run() {
-//     console.log(this);
-// }
-// run();
+function run() {
+    console.log(this);
+}
+run();
 
 
-console.log("hey")
+// console.log("hey")
 
